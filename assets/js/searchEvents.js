a20cc9d012a8ece0337c29d12451a8f29f3486db
ardayasar <erddoerdo12@gmail.com>
2021-09-30 08:08:42 +0300

    a

--- assets/js/searchEvents.js
+++ assets/js/searchEvents.js
@@ -8,7 +8,6 @@ $("#search").on("input", function(){
         }
     })
     var search = $("#search").val();
-    let activeList = [];
     if(search.length > 0){
         $.get('dates.json', function(data) {
             $dat = JSON.parse(data);
@@ -17,11 +16,10 @@ $("#search").on("input", function(){
                     $(".bg-shadow").append('<div class="panel">   <h4> ' + element["news"] + ' </h4><br><div class="date"><p> ' + element["date"] + ' </p></div></div>');
                 }
             });
-            //$(".calendar").append("<div class='day'><p class='date-of-day'>" + d + "</p><p class='event-dots'> "+ $f + "</p></div>");
         }, 'text');
     }
     else{
-
+        
     }
 
 })
